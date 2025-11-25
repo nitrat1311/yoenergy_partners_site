34a19f76ed4e9cd933911986a722a6107ff9f655
nitrat1311 <88509809+nitrat1311@users.noreply.github.com>
2025-11-25 20:26:43 +0700

    Build docs output for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
+      base: '/yoenergy_partners_site/',
       server: {
         port: 3000,
         host: '0.0.0.0',
+      },
+      build: {
+        outDir: 'docs',
       },
       plugins: [react()],
       define: {
